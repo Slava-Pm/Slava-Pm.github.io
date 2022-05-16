533cb6b4bc52772ef74aa9fdbac8b63fbe89d568
Slava-Pm <103124918+Slava-Pm@users.noreply.github.com>
2022-05-16 14:45:06 +0300

    js

--- laba2/lab2.js
+++ laba2/lab2.js
@@ -20,5 +20,12 @@ function factorial(n){
   }
   //Функция возвращает n-ое число Фибонначи
 function fib(n){
-    return n <= 1 ? n : fib(n - 1) + fib(n - 2);   
-}  
+        let a = 1;
+        let b = 1;
+        for (let i = 3; i <= n; i++) {
+          let c = a + b;
+          a = b;
+          b = c;
+        }
+        return b;
+      }
