260fab92c2c693907847878ec346b60c9281603b
Slava-Pm <103124918+Slava-Pm@users.noreply.github.com>
2022-06-07 12:59:35 +0300

    ап

--- laba2/lab2.js
+++ laba2/lab2.js
@@ -75,3 +75,23 @@ function compare(x)
     }}
 
     pow(2, 3)
+/**
+ * 
+ * @param {number} num - число
+ * @param {number} cols - к-во столбцов 
+ * @returns {string} - на вывод
+ * 
+ */
+ function printNumbers(num, cols) {
+  let str1 = ""
+  let rows = Math.ceil(num / cols); //кол-во строк округлено в большую сторону
+  for (let row = 0; row < rows; ++row) {
+      let str = '';
+      for (let col = 0; col < cols; ++col) {
+          if ((row + rows * col) < num)
+              str += (row + rows * col) + ' ';
+      }
+      str1+=str+"\n";       
+  }
+  return str1;
+}
