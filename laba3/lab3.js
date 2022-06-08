8de61b3e8770b6d4971ee3f63076196c484bd34c
Slava-Pm <103124918+Slava-Pm@users.noreply.github.com>
2022-06-08 10:15:04 +0300

    ап

---
+++ laba3/lab3.js
@@ -0,0 +1,100 @@
+'use strict'
+/**
+ * 
+ * @param {number} num - число 
+ * @returns - число
+ */
+function getDecimal(num) {
+    return num - Math.floor(num); //округление вниз 
+}
+/**
+ * 
+ * @param {string} str - строка
+ * @returns 
+ */
+function ucFirst(str) {
+    // делаем заглавный первый символ и конкатенируем с остальной строкой
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+/**
+ * 
+ * @param {string} str - строка
+ * @returns 
+ */
+function checkSpam(str) {
+    let strToLower = str.toLowerCase();
+    if (strToLower.includes('viagra') ||
+        strToLower.includes('xxx')) {
+        return true;
+    }
+    return false;
+}
+/**
+ * 
+ * @param {string} str - строка
+ * @param {nu} maxlength - длина строки
+ * @returns 
+ */
+function truncate(str, maxlength) {
+    if (str.length > maxlength)
+        str = str.slice(0, maxlength - 1) + '\u{2026}'//мнгоготочие;
+    return str;
+}
+/**
+ * 
+ * @param {string} str - строка
+ * @returns 
+ */
+function camelize(str) {
+    return str
+        .split('-')
+        .map((word, index) => index == 0 ? word : word[0].toUpperCase() + word.slice(1))
+        .join('');
+}
+/**
+ * 
+ * @param {array} N - массив
+ * @returns 
+ */
+function fibs(N) {
+    let a = [];
+    let str = '';
+    if (!Number.isNaN(N))
+        if (N == 0) str = 0;
+        else
+            for (let p = 0; p < N; ++p) {
+                a[p] = fib(p);
+                str += a[p] + ' ';
+            }
+    else str = NaN;
+
+    return str;
+
+    function fib(p) {
+        let a = 1n;
+        let b = 1n;
+        //Исключения
+        if (p == 0) return 0;
+        if (p == 1) return 1;
+        if (p == 2) return 1;
+        for (let i = 3; i <= p; i++) {
+            let c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
+    }
+}
+/**
+ * 
+ * @returns - сумма всех аргументов
+ */
+function sum() {
+    let result = 0;
+
+    for (let i = 0; i < arguments.length; i++) {
+        result += arguments[i];
+    }
+
+    return result;
+}
