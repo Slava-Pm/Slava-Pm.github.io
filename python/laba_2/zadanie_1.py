209d4580bdd4ac0346211820d3a37348535c2deb
Slava-Pm <103124918+Slava-Pm@users.noreply.github.com>
2022-10-14 18:35:07 +0300

    Create zadanie_1.py

---
+++ python/laba_2/zadanie_1.py
@@ -0,0 +1,17 @@
+a, b, c = map(int,input().split())
+if a>b and a>c and c>b:
+    print(a,c,b)
+elif a>b and a>c and b>c
+    print(a,b,c)
+elif b>a and b>c and a>c
+    print(b,a,c)
+elif b>a and b>c and c>a
+    print(b,c,a)
+elif c>a and c>b and b>a
+    print(c,b,a)
+elif c>a and c>b and a>b
+    print(c,a,b)
+else: 
+    print(a,"=",b,"=",c)
+                
+            
