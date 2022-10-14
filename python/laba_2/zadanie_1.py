59e0d2f4f359c7d96e83d7776565148d505098a9
Slava-Pm <103124918+Slava-Pm@users.noreply.github.com>
2022-10-14 18:50:52 +0300

    Update zadanie_1.py

--- python/laba_2/zadanie_1.py
+++ python/laba_2/zadanie_1.py
@@ -1,17 +1,23 @@
+print("Введите три числа")
 a, b, c = map(int,input().split())
 if a>b and a>c and c>b:
     print(a,c,b)
-elif a>b and a>c and b>c
+elif a>b and a>c and b>c:
     print(a,b,c)
-elif b>a and b>c and a>c
+elif b>a and b>c and a>c:
     print(b,a,c)
-elif b>a and b>c and c>a
+elif b>a and b>c and c>a:
     print(b,c,a)
-elif c>a and c>b and b>a
+elif c>a and c>b and b>a:
     print(c,b,a)
-elif c>a and c>b and a>b
+elif c>a and c>b and a>b:
     print(c,a,b)
-else: 
+elif (a==b or a==c or b==c) and a>c: 
+    print(a,b,c)
+elif (a==b or a==c or b==c) and b>c: 
+    print(b,c,a) 
+elif (a==b or a==c or b==c) and c>a: 
+    print(c,a,b) 
+else:
     print(a,"=",b,"=",c)
                 
-            
