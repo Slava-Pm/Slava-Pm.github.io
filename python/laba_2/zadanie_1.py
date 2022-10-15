56334f6ac04c420c94f26a6dfe13cee0f5b495b1
Slava-Pm <103124918+Slava-Pm@users.noreply.github.com>
2022-10-15 13:39:39 +0300

    Update zadanie_1.py

--- python/laba_2/zadanie_1.py
+++ python/laba_2/zadanie_1.py
@@ -1,23 +1,17 @@
 print("Введите три числа")
-a, b, c = map(int,input().split())
-if a>b and a>c and c>b:
+a, b, c = map(float,input().split())
+if (a>b and a>c and c>b) or (a==c and a>b):
     print(a,c,b)
-elif a>b and a>c and b>c:
+elif (a>b and a>c and b>c) or (a==b and a>c) or (b==c and b<a):
     print(a,b,c)
-elif b>a and b>c and a>c:
+elif (b>a and b>c and a>c) or (a==c and a<b):
     print(b,a,c)
-elif b>a and b>c and c>a:
+elif (b>a and b>c and c>a) or (b==c and b>a):
     print(b,c,a)
 elif c>a and c>b and b>a:
     print(c,b,a)
-elif c>a and c>b and a>b:
+elif (c>a and c>b and a>b) or (a==b and a<c):
     print(c,a,b)
-elif (a==b or a==c or b==c) and a>c: 
-    print(a,b,c)
-elif (a==b or a==c or b==c) and b>c: 
-    print(b,c,a) 
-elif (a==b or a==c or b==c) and c>a: 
-    print(c,a,b) 
+# Если все три числа имеют одинаковое значение
 else:
     print(a,"=",b,"=",c)
-                
