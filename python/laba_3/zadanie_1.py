1af7a6a2600da188ce1cf04075be6daf62a3e3ba
Slava-Pm <103124918+Slava-Pm@users.noreply.github.com>
2022-10-15 17:11:42 +0300

    Update zadanie_1.py

--- python/laba_3/zadanie_1.py
+++ python/laba_3/zadanie_1.py
@@ -1,1 +1,13 @@
-
+import math
+n = int(input('Введите число:'))
+flag = True
+if (n%2==0):
+    flag = False
+if n > 0:
+    digits = int(math.log10(n))+1
+elif n == 0:
+    digits = 1
+else:
+    digits = int(math.log10(-n))+1
+print('Цифр в числе:',digits)
+print(flag)
