5e1414684227232c2135378957a0e8aa69952c08
Slava-Pm <103124918+Slava-Pm@users.noreply.github.com>
2022-10-15 18:49:37 +0300

    Update zadanie_2.py

--- python/laba_3/zadanie_2.py
+++ python/laba_3/zadanie_2.py
@@ -1,1 +1,11 @@
-
+import math 
+from random import randint
+A,B = map(int(input('Введите два числа: ')).split())
+dr == 3
+n = randint(100,999)
+if (dr==int(math.log10(n))+1):
+    print("Коректное число")
+else:
+    print("Проверьте правильность введённого числа")
+    
+    
