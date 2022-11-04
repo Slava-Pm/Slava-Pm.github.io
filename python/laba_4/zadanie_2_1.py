93b8d7b32f80d450b0ecbc8d3eb0097a64a6dbd5
Slava-Pm <103124918+Slava-Pm@users.noreply.github.com>
2022-11-04 15:27:25 +0300

    Update zadanie_2_1.py

--- python/laba_4/zadanie_2_1.py
+++ python/laba_4/zadanie_2_1.py
@@ -1,1 +1,17 @@
+'''В одномерном массиве, состоящем из п целых элементов удалить 
+все отрицательные элементы.'''
 
+n = int(input())
+
+arr = []
+for i in range(n):
+    arr.append(int(input()))
+
+i = 0
+delarr = []  # массив индексов по которым в arr надо удалить элементы
+for i in range(n):
+    if arr[i] < 0:
+        delarr.append(i)
+for i in range(len(delarr)):
+    del arr[delarr[i]]
+print(*arr)
