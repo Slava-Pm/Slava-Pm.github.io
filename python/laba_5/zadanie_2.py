554a9716f7e3819505805463815e26a4cbca28f7
Slava-Pm <103124918+Slava-Pm@users.noreply.github.com>
2022-11-08 18:24:56 +0300

    Update zadanie_2.py

--- python/laba_5/zadanie_2.py
+++ python/laba_5/zadanie_2.py
@@ -29,3 +29,89 @@ for i in range(m):
     for j in range(n):
         print(matrix[i][j], end=' ')
     print()
+
+'''Дана матрица размера M × N. Удалить ее последний
+столбец, содержащий только отрицательные элементы.'''
+    
+    
+n = int(input())
+m = int(input())
+
+matrix = [0]*n;
+for i in range(n):
+    matrix[i] = [0]*m
+    
+for i in range(n):
+    for j in range(m):
+        matrix[i][j] = int(input())
+    
+jmax = 0
+for j in range(m):
+    k = 0
+    for i in range(n):
+        if matrix[i][j] < 0:
+            k += 1
+    if k == n:
+        jmax = j
+        
+for i in range(n):
+    del matrix[i][jmax]
+    
+for i in range(n):
+    for j in range(m-1):
+        print(matrix[i][j], end = " ")
+    print()  
+    
+'''Дана матрица размера M × N. Вставить в нее строку из
+некоторого числа K перед строкой, среднеарифметическое
+значение элементов которой максимально.'''
+
+n = int(input())
+m = int(input())
+
+matrix = [0]*n;
+for i in range(n):
+    matrix[i] = [0]*m
+    
+for i in range(n):
+    for j in range(m):
+        matrix[i][j] = int(input())
+    
+k = int(input())
+
+maxavg = 0
+for j in range(n):
+    maxavg += matrix[0][j]
+maxavg /= m
+imaxavg = 0
+
+for i in range(n):
+    avg = 0
+    for j in range(m):
+        avg += matrix[i][j]
+    avg /= m
+    if avg> maxavg:
+        maxavg = avg
+        imaxavg = i
+        
+matrix1 = [0]*(n+1);
+for i in range(n+1):
+    matrix1[i] = [0]*m
+        
+for i in range(imaxavg):
+    for j in range(m):
+        matrix1[i][j] = matrix[i][j]      
+        
+for j in range(m):
+    matrix1[imaxavg][j] = k
+
+for i in range(imaxavg, n):
+    for j in range(m):
+        matrix1[i+1][j] = matrix[i][j]
+        
+# Итоговая матрица
+        
+for i in range(n+1):
+    for j in range(m):
+        print(matrix1[i][j], end=" ")
+    print()
