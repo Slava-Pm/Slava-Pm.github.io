b6581c53f0b92ad71d21f5265639878ce9f290bb
Slava-Pm <103124918+Slava-Pm@users.noreply.github.com>
2022-11-08 13:00:51 +0300

    Update zadanie_3.py

--- python/laba_5/zadanie_3.py
+++ python/laba_5/zadanie_3.py
@@ -1,9 +1,6 @@
 '''Дана целочисленная матрица A(N, N), заполненная целыми
 числами. Все ее положительные элементы записать в
-одномерный массив, а остальные в другой.
-Дана целочисленная матрица A(N, N), заполненная целыми
-числами. Отсортировать по убыванию элементы матрицы,
-расположенные под главной диагональю.'''
+одномерный массив, а остальные в другой.'''
 
 n = int(input())
 
@@ -30,3 +27,42 @@ for i in range(n):
     for j in range(n):
         print(matrix[i][j], end=" ")
     print()
+
+'''Дана целочисленная матрица A(N, N), заполненная целыми
+числами. Отсортировать по убыванию элементы матрицы,
+расположенные над главной диагональю.'''
+
+n = int(input())
+
+matrix = [0] * n
+for i in range(n):
+    matrix[i] = [0] * n
+for i in range(n):
+    for j in range(n):
+        matrix[i][j] = int(input())
+print("первоначальная матрица: ")
+for i in range(n):
+    for j in range(n):
+        print(matrix[i][j], end=' ')
+    print()
+
+
+arr3 = []
+
+for i in range(n):
+    for j in range(n):
+        if j > i:
+            arr3.append(matrix[i][j])
+arr3.sort(reverse=True)
+k = 0
+for i in range(n):
+    for j in range(i, n):
+        if j > i:
+            matrix[i][j] = arr3[k]
+            k += 1
+
+print('Решение : ')
+for i in range(n):
+    for j in range(n):
+        print(matrix[i][j], end =' ')
+    print()    
