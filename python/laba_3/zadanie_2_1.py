c55df3730081facf083408d43f1a969bd7bf20e4
Slava-Pm <103124918+Slava-Pm@users.noreply.github.com>
2022-11-09 12:51:01 +0300

    Update zadanie_2_1.py

--- python/laba_3/zadanie_2_1.py
+++ python/laba_3/zadanie_2_1.py
@@ -3,9 +3,9 @@
 
 n = int(input("Введите число n "))
 for i in range(1,200):
-    counter = 0
-    for j in range(1,int(i / 2) +1):
+    counter = 2
+    for j in range(2,int(i / 2) +1):
         if i % j == 0:
             counter += 1
-    if counter + 1  == n:
-        print('Число :',i,'Делители: ',counter+1)
+    if counter  == n:
+        print('Число :',i,'Делители: ',counter)
