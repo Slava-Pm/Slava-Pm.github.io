09334f54afcb6c22d6354a7b064058d41eae210f
Slava-Pm <103124918+Slava-Pm@users.noreply.github.com>
2022-11-22 23:02:01 +0300

    Update zadanie_1.py

--- python/laba_6/zadanie_1.py
+++ python/laba_6/zadanie_1.py
@@ -1,1 +1,15 @@
-в
+'''Рассчитать значение z=max(a,2b)+max(2a-b,b) определив и использовав
+функцию max(x,y) – максимальное из двух чисел.'''
+
+def maxi(a, b):
+    if a > b:
+        return a
+    else:
+        return b
+
+
+a = int(input("Введите а: "))
+b = int(input("Введите b: "))
+z = 0
+z = maxi(a, 2*b) + maxi(2 * a -b,b)
+print(z)
