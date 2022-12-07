806413be2263ce9b70675a67a78d634a2cd7660d
Slava-Pm <103124918+Slava-Pm@users.noreply.github.com>
2022-12-07 10:22:27 +0300

    Update zadanie_3.py

--- python/laba_6/zadanie_3.py
+++ python/laba_6/zadanie_3.py
@@ -1,3 +1,8 @@
+'''Описать рекурсивную функцию NOD(A,B) целого типа, находящую наибольший общий делитель двух натуральных чисел A и B, 
+используя алгоритм Евклида: NOD(A,B) = NOD(B mod A,A), если A <> 0; NOD(0,B) =
+B. С помощью этой функции найти наибольшие общие делители пар A и B,
+A и C, A и D, если даны числа A, B, C, D.'''
+
 def gcd_recursion(num1, num2):
     if num1 == 0:
         return num2
@@ -5,3 +10,5 @@ def gcd_recursion(num1, num2):
 
 
 print(gcd_recursion(102, 256))
+print(gcd_recursion(102, 306))
+print(gcd_recursion(102, 482))
